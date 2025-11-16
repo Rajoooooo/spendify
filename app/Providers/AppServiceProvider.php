9b580083d8129dc90cb2b3bf6268834c14d3aded
rajoooo <158122672+Rajoooooo@users.noreply.github.com>
2025-11-16 12:19:44 +0000

    Refactor AppServiceProvider boot method and add Nginx configuration for Azure deploymentl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,8 +17,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+   public function boot(): void
     {
-        //
+        Vite::prefetch(concurrency: 3);
+        if ($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
